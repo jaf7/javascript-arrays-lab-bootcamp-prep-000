9f788a247d475ee847ea54bd0e42ec92ed1bace1
Anthony Fields <janthonyfields@gmail.com>
2017-09-09 20:24:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function destructivelyAppendKitten( name ) {
   kittens.push( name );
   return kittens;
 }
+
+function destructivelyPrependKitten( name ) {
+  kittens.shift( name );
+  return kittens;
+}
